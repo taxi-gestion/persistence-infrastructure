95dc4d658cc1ceaae222b7b7830afda10fcd1eb4
Romain Cambonie <romain.cambonie@gmail.com>
2023-05-25 11:02:07 +0200

    feat: add route_association_open_rds

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,11 @@
+# Ephemeral: Open the RDS instance to the internet for data migration
+resource "aws_route_table_association" "route_association_open_rds" {
+  count          = var.open_rds_to_public_internet ? 1 : 0
+  subnet_id      = var.private_subnets_ids[0] // Must match the subnet in the same AZ as the instance
+  route_table_id = var.public_route_table_id
+}
+
+
 resource "aws_db_subnet_group" "private_subnets_group" {
   name       = "private-subnets-for-rds"
   subnet_ids = var.private_subnets_ids
